3c704adf9108ec1cca9264b92b5da7816d959096
dharmik3504 <Heavynotebook@gmail.com>
2022-11-26 12:38:10 +0530

    added create_userLogic
    added create_userLogic and db logic

---
+++ environment.js
@@ -0,0 +1,4 @@
+const db_link = "http://localhost:3000";
+const api_link = "http://localhost:3001";
+api_port = 3001;
+module.exports = { db_link, api_port, api_port };
--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const { v4: uuidv4 } = require("uuid");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json({ limit: "500mb" }));
-const port = 3000;
+
+const create_userLogic = require("./create_user");
+const dealLogic = require("./deal");
+
+const { api_port } = require("./environment");
 app.get("/", (req, res) => {
   res.send(res.statusCode);
 });
-app.post("/api/v1/create_user", (req, res) => {
+app.post("/api/v1/create_user", async (req, res) => {
   var { name, coins } = req.query;
-  const userId = "U_" + uuidv4();
-  res.send(userId);
+  const create_userLogicResponse = await create_userLogic(name, coins);
+  res.send(create_userLogicResponse);
 });
 app.post("/api/v1/create_game", (req, res) => {
   var { players, decks } = req.body;
@@ -18,6 +22,12 @@ app.post("/api/v1/create_game", (req, res) => {
   const gameId = "G_" + uuidv4();
   res.send(gameId);
 });
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.post("/api/v1/deal", (req, res) => {
+  var { id, coins } = req.body;
+  const dealResponse = dealLogic(game_id, coins);
+  const gameId = "G_" + uuidv4();
+  res.send(gameId);
+});
+app.listen(api_port, () => {
+  console.log(`Example app listening at http://localhost:${api_port}`);
 });
